c79e0bcaad4f613cec93a6fb55ad8e49fa8cdfbb
Wesley Silva <wesleysilva059@gmail.com>
2020-06-09 00:36:20 -0300

    atualização

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -4,6 +4,7 @@
   <head>
     <meta charset="UTF-8">
     <title>Menu Digital - Açougue da Tina</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!--<link rel="stylesheet" type="text/css" href="css/bootstrap-theme.min.css">-->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
     <style type="text/css">
@@ -15,177 +16,9 @@
 
     </style>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    {{-- <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script> --}}
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-    <style type="text/css">
-
-      .full {
-        height: 100%;
-        background-image: url('/images/fundo-madeira.jpg'); 
-        background-size: 1920px 1080px;
-        width: 100%;
-      }
-
-      html,
-      body {
-        overflow-y: hidden;
-        overflow-x: hidden;
-        height: 100%;
-      }
-
-      html, body, div, span, applet, object, iframe,
-      h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-      a, abbr, acronym, address, big, cite, code,
-      del, dfn, em, img, ins, kbd, q, s, samp,
-      small, strike, strong, sub, sup, tt, var,
-      b, u, i, center,
-      dl, dt, dd, ol, ul, li,
-      fieldset, form, label, legend,
-      table, caption, tbody, tfoot, thead, tr, th, td,
-      article, aside, canvas, details, embed, 
-      figure, figcaption, footer, header, hgroup, 
-      menu, nav, output, ruby, section, summary,
-      time, mark, audio, video { {
-        margin: 0;
-        padding: 0;
-        border: 0;
-        font-size: 100%;
-        font: inherit;
-        vertical-align: baseline;
-      }
-
-      * {
-        box-sizing: border-box;
-      }
-      /* HTML5 display-role reset for older browsers */
-
-      article, aside,details,figcaption,figure,footer,header,hgroup,menu,nav,
-      section {
-        display: block;
-      }
-
-      body {
-        line-height: 1;
-      }
-
-      ol,
-      ul {
-        list-style: none;
-      }
-
-      blockquote,
-      q {
-        quotes: none;
-      }
-
-      blockquote:before,
-      blockquote:after,
-      q:before,
-      q:after {
-        content: '';
-        content: none;
-      }
-
-      table {
-        border-collapse: collapse;
-        border-spacing: 0;
-      }
-      /* comeco fade carousel */
-
-      .carousel-fade .carousel-inner .item {
-        opacity: 0;
-        transition-property: opacity;
-      }
-
-      .carousel-fade .carousel-inner .active {
-        opacity: 1;
-      }
-
-      .carousel-fade .carousel-inner .active.left,
-      .carousel-fade .carousel-inner .active.right {
-        left: 0;
-        opacity: 0;
-        z-index: 1;
-      }
-
-      .carousel-fade .carousel-inner .next.left,
-      .carousel-fade .carousel-inner .prev.right {
-        opacity: 1;
-      }
-
-      .carousel-fade .carousel-control {
-        z-index: 2;
-      }
-
-      @media all and (transform-3d),
-      (-webkit-transform-3d) {
-        .carousel-fade .carousel-inner > .item.next,
-        .carousel-fade .carousel-inner > .item.active.right {
-          opacity: 0;
-          -webkit-transform: translate3d(0, 0, 0);
-          transform: translate3d(0, 0, 0);
-        }
-        .carousel-fade .carousel-inner > .item.prev,
-        .carousel-fade .carousel-inner > .item.active.left {
-          opacity: 0;
-          -webkit-transform: translate3d(0, 0, 0);
-          transform: translate3d(0, 0, 0);
-        }
-        .carousel-fade .carousel-inner > .item.next.left,
-        .carousel-fade .carousel-inner > .item.prev.right,
-        .carousel-fade .carousel-inner > .item.active {
-          opacity: 1;
-          -webkit-transform: translate3d(0, 0, 0);
-          transform: translate3d(0, 0, 0);
-        }
-      }
-      /* fim fade carousel */
-
-      .cor {
-        position: absolute;
-        background-color: rgba(255, 255, 255, 0.6);
-        z-index: 10;
-        left: 50%;
-        margin-left: -615px;
-        height: 70px;
-        top: 2%;
-      }
-
-      .nav-justified>li {
-        width: 200px;
-        padding: 5px;
-      }
-
-      .nav-justified {
-        margin-top: 13px;
-      }
-
-      .nav-pills>li>a {
-        color: #000;
-        border-radius: 0;
-        text-transform: uppercase;
-        font-family: tahoma verdana;
-        font-style: italic;
-      }
-
-      .nav-pills>li>a:hover {
-        border-radius: 0;
-        background-color: #262b31;
-        color: #fff;
-      }
-
-      .nav-pills>li.active>a,
-      .nav-pills>li.active>a:focus,
-      .nav-pills>li.active>a:hover {
-        background-color: #262b31;
-        color: #fff;
-      }
-
-      td{
-         color: #fff;
-      }
-
-    </style>
+    <link rel="stylesheet" href="/css/stylefront.css">
   </head>
 
   <body>
@@ -202,47 +35,62 @@
           <!-- Wrapper for slides -->
           <div class="carousel-inner full" role="listbox">
             <div class="item active">
-              <h1>Slide1</h1>
-              <table class="table">
-                 <thead>
-                    <tr>
-                       <th>teste</th>
-                       <th>tesdt</th>
-                       <th>asdtsat</th>
-                    </tr>
-                 </thead>
-                 <tbody>
-                    <tr>
-                       <td scope="row">asdtas</td>
-                       <td>asdtsadt</td>
-                       <td>asdtadta</td>
-                    </tr>
-                    <tr>
-                       <td scope="row">asdtasdt</td>
-                       <td>asdtasdt</td>
-                       <td>asdtt</td>
-                    </tr>
-                 </tbody>
-              </table>
+              <div class="col-md-4 promocao">
+              </div>
+              <div class="col-md-8 produtos">
+                 <div class="logo-principal">
+                  <figure>
+                     <img src="/images/logo.png" class="img-responsive center-block" alt="" srcset="">
+                  </figure>
+                 </div>
+                 <div class="titulo">
+                    <h1>Bovinos</h1>
+                 </div>
+                 @for ($i = 0; $i < 8; $i++)
+                 <div class="list-produtos">
+                  <div class="col-md-9">Picanha</div>
+                  <div class="col-md-3">R$ 29,90</div>
+               </div>    
+                 @endfor
+                 
+              </div>
             </div>
             <div class="item">
-               <h1>Slide2</h1>
+               <div class="col-md-4 promocao">
+               </div>
+               <div class="col-md-8 produtos">
+                <figure>
+                   <img src="/images/logo.png" class="logo" alt="" srcset="">
+                </figure>
+               </div>
             </div>
             <div class="item">
-               <h1>Slide3</h1>
+               <div class="col-md-4 promocao">
+               </div>
+               <div class="col-md-8 produtos">
+                <figure>
+                   <img src="/images/logo.png" class="logo" alt="" srcset="">
+                </figure>
+               </div>
             </div>
             <div class="item">
-               <h1>Slide4</h1>
+               <div class="col-md-4 promocao">
+               </div>
+               <div class="col-md-8 produtos">
+                <figure>
+                   <img src="/images/logo.png" class="logo" alt="" srcset="">
+                </figure>
+               </div>
              </div>
           </div>
         </div>
       </div>
     </div>
-    <script type="text/javascript" src="js/jquery-2.2.3.min.js"></script>
-    <script type="text/javascript" src="js/bootstrap.js"></script>
+    <script type="text/javascript" src="/js/jquery-2.2.3.min.js"></script>
+    <script type="text/javascript" src="/js/bootstrap.js"></script>
     <script type="text/javascript">
       $('.carousel').carousel({
-        interval: 3000
+        interval: 300000
       });
 
     </script>
